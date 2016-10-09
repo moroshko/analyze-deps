90e6d52d7f5a87a142b6937c6ad417e1d1aeb5aa
Misha Moroshko <michael.moroshko@gmail.com>
2016-10-09 23:41:40 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Analyze Deps
+
+Compares dependencies in the specified `package.json` to the latest available versions.
+
+## Installation
+
+```shell
+npm install analyze-deps --save
+```
+
+## Usage
+
+```js
+const analyzeDeps = require('analyze-deps');
+const packageJson = {
+  name: 'analyze-deps',
+  license: 'MIT',
+  dependencies: {
+    'lodash.mapvalues': "^4.6.0",
+    'package-json': "^1.2.0",
+    'promise-all': "^1.0.0",
+    'semver': "^5.2.0"
+  },
+  devDependencies: {
+    'eslint': "^3.7.0"
+  }
+};
+
+analyzeDeps(packageJson).then(analysis => console.log(analysis));
+/*
+  {
+    dependencies: {
+      'lodash.mapvalues': {
+        status: 'latest'
+      },
+      'package-json': {
+        status: 'not-latest',
+        current: '^1.2.0',
+        latest: '2.4.0',
+        diff: 'major'
+      },
+      'promise-all': {
+        status: 'latest'
+      },
+      semver: {
+        status: 'not-latest',
+        current: '^5.2.0',
+        latest: '5.3.0',
+        diff: 'minor'
+      }
+    },
+    devDependencies: {
+      eslint: {
+        status: 'not-latest',
+        current: '^3.7.0',
+        latest: '3.7.1',
+        diff: 'patch'
+      }
+    }
+  }
+*/
+```
---
+++ index.js
@@ -0,0 +1,83 @@
+const semver = require('semver');
+const mapValues = require('lodash.mapvalues');
+const promiseAll = require('promise-all');
+const getPackageData = require('package-json');
+
+const nonDigitStartRegex = /^[^0-9]+/;
+
+const getDiff = (range, version) => {
+  if (range.indexOf(' ') !== -1) {
+    return '';
+  }
+
+  const rangeVersion = range.replace(nonDigitStartRegex, '');
+
+  if (!semver.valid(rangeVersion)) {
+    return '';
+  }
+
+  return semver.diff(rangeVersion, version) || '';
+};
+
+const analyzePackage = (packageName, range) =>
+  getPackageData(packageName).then(result => {
+    const latestVersion = result['dist-tags'].latest;
+    const versions = Object.keys(result.versions);
+    const versionsInRange = versions.filter(version =>
+      semver.satisfies(version, range)
+    );
+
+    if (versionsInRange.length === 0) {
+      return {
+        status: 'error',
+        error: `Package \`${packageName}\` doesn't have versions in range ${range}. Latest version is ${latestVersion}.`
+      };
+    }
+
+    if (versionsInRange.length > 1 || versionsInRange[0] !== latestVersion) {
+      return {
+        status: 'not-latest',
+        current: range,
+        latest: latestVersion,
+        diff: getDiff(range, latestVersion)
+      };
+    }
+
+    return {
+      status: 'latest'
+    };
+  })
+  .catch(err => {
+    return {
+      status: 'error',
+      error: err.message
+    };
+  });
+
+const analyzeDeps = deps => {
+  const tasks = mapValues(deps, (range, packageName) =>
+    analyzePackage(packageName, range)
+  );
+
+  return promiseAll(tasks);
+};
+
+const defaultOptions = {
+  dependencies: true,
+  devDependencies: true
+};
+
+const analyzeAllDeps = (packageJson, options) => {
+  const mergedOptions = Object.assign({}, defaultOptions, options);
+  const tasks = ['dependencies', 'devDependencies'].reduce((result, key) => {
+    if (packageJson[key] && mergedOptions[key]) {
+      result[key] = analyzeDeps(packageJson[key]);
+    }
+
+    return result;
+  }, {});
+
+  return promiseAll(tasks);
+};
+
+module.exports = analyzeAllDeps;
